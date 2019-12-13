66254ae499d7661b5f38d78df5c91960b7123029
Steve Sims <stevenalexandersims@gmail.com>
2019-12-12 20:30:32 -0800

    FetchUrlWorker fetches the URL asynchronously

--- app/controllers/api/v1/image_parser_controller.rb
+++ app/controllers/api/v1/image_parser_controller.rb
@@ -1,24 +1,17 @@
+require 'securerandom'
+
 class Api::V1::ImageParserController < ApplicationController
 
-  #TODO(stevesims330): Make it so this doesn't block the thread by kicking it off into a job.
-
-  # TODO(stevesims330): Most of this logic will go into a job. begin_fetch will merely kick off the job and return an ID.
   def begin_fetch
-    begin
-      response = Faraday.get(params["document_url"])
-    rescue Faraday::ConnectionFailed
-      return render status: :bad_request, json: "Could not load #{params["document_url"]}. Perhaps the site is down or the URL is mistyped."
-    end
+    request_id = SecureRandom.uuid
+    FetchUrlWorker.perform_async(request_id, params["url"])
 
-    begin
-      image_url = ImageParsingService.new(response.body).url
-    rescue OGP::MalformedSourceError => e
-      return render status: :bad_request, json: "No Open Graph og:image attribute found."
-    end
-
-    render status: :ok, json: image_url.to_json
+    render status: :ok, json: request_id.to_json
   end
 
-  # def get_fetched_url
+  def get_fetched_url
+    result = Redis.new.get(params["request_id"])
+    render status: :ok, json: result
+  end
 
 end
--- app/workers/fetch_url_worker.rb
+++ app/workers/fetch_url_worker.rb
@@ -1,9 +1,21 @@
 class FetchUrlWorker
   include Sidekiq::Worker
 
-  def perform(request_id)
-    hello_world = "FetchUrlWorker.perform called. request_id: #{request_id}"
-    Rails.logger.info(hello_world)
-    puts hello_world
+  def perform(request_id, document_url)
+    result = { :status => 200, :url => nil, error: nil }
+    begin
+      response = Faraday.get(document_url)
+      result[:url] = ImageParsingService.new(response.body).url
+    rescue Faraday::ConnectionFailed
+      result[:status] = 400
+      result[:error] = "Could not load #{document_url}. Perhaps the site is down or the URL is mistyped."
+    rescue OGP::MalformedSourceError => e
+      result[:status] = 400
+      result[:error] = "No Open Graph og:image attribute found."
+    end
+
+    #TODO(stevesims330): Write a Redis config file
+    Redis.new.set(request_id, result)
+    result
   end
 end
