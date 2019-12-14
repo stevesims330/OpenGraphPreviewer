23babb033e632c45fabf303759e3c9e2a52b272e
Steve Sims <stevenalexandersims@gmail.com>
2019-12-13 18:35:56 -0800

    Generated placeholder React app

---
+++ app/controllers/open_graph_previewer_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class OpenGraphPreviewerController < ApplicationController
+  #TODO(stevesims330): Make a CSS file
+  #layout "open_graph_previewer"
+
+  def index
+  end
+end
---
+++ app/javascript/bundles/OpenGraphPreviewer/components/OpenGraphPreviewer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class OpenGraphPreviewer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+}
